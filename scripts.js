6da9d04aab4f8683461b3386f04f4e90136afab1
Kai <ali.rf.kashani@gmail.com>
2021-10-21 17:27:15 -0400

    added rules modal, finished game functionality and responsive styling

--- scripts.js
+++ scripts.js
@@ -8,6 +8,8 @@ const handOptions = {
 	scissors: scissorsHtml,
 }
 
+let SCORE = 0
+
 const pickUserHand = (hand) => {
 	let hands = document.querySelector('.start')
 	hands.style.display = 'none'
@@ -18,13 +20,77 @@ const pickUserHand = (hand) => {
 	// set the user pick
 	document.querySelector('.userPickImage').innerHTML = handOptions[hand]
 
-	pickComputerHand()
+	let cpHand = pickComputerHand(hand)
+	referee(hand, cpHand)
 }
 
-const pickComputerHand = () => {
+const pickComputerHand = (hand) => {
 	let hands = ['rock', 'paper', 'scissors']
-
 	let cpHand = hands[Math.floor(Math.random() * hands.length)]
 
 	document.querySelector('.compPickImage').innerHTML = handOptions[cpHand]
+
+	return cpHand
+}
+
+const referee = (userHand, cpHand) => {
+	if (userHand == 'paper' && cpHand == 'scissors') {
+		setDecision('YOU LOSE!')
+	}
+	if (userHand == 'paper' && cpHand == 'rock') {
+		setDecision('YOU WIN!')
+		setScore(SCORE + 1)
+	}
+	if (userHand == 'paper' && cpHand == 'paper') {
+		setDecision("It's a tie!")
+	}
+	if (userHand == 'rock' && cpHand == 'scissors') {
+		setDecision('YOU WIN!')
+		setScore(SCORE + 1)
+	}
+	if (userHand == 'rock' && cpHand == 'paper') {
+		setDecision('YOU LOSE!')
+	}
+	if (userHand == 'rock' && cpHand == 'rock') {
+		setDecision("It's a tie!")
+	}
+	if (userHand == 'scissors' && cpHand == 'scissors') {
+		setDecision("It's a tie!")
+	}
+	if (userHand == 'scissors' && cpHand == 'rock') {
+		setDecision('YOU LOSE!')
+	}
+	if (userHand == 'scissors' && cpHand == 'paper') {
+		setDecision('YOU WIN!')
+		setScore(SCORE + 1)
+	}
+}
+
+const restartGame = () => {
+	let contest = document.querySelector('.contest')
+	contest.style.display = 'none'
+
+	let hands = document.querySelector('.start')
+	hands.style.display = 'flex'
+}
+
+const setDecision = (decision) => {
+	document.querySelector('.decision .contest__title').innerText = decision
+	// console.log(decision)
+}
+
+const setScore = (newScore) => {
+	SCORE = newScore
+	document.querySelector('.score').innerText = newScore
+	// console.log(newScore)
+}
+
+const openBtn = () => {
+	const openModal = document.querySelector('.rules-modal')
+	openModal.style.display = 'flex'
+}
+
+const closeBtn = () => {
+	const closeModal = document.querySelector('.rules-modal')
+	closeModal.style.display = 'none'
 }
